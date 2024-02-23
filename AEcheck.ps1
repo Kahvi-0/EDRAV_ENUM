026a22594bea42cd92adf3f0c24ad8f814ab4d17
Kahvi-0xFF <46513413+Kahvi-0@users.noreply.github.com>
2024-02-23 11:29:35 -0500

    Update AEcheck.ps1

--- AEcheck.ps1
+++ AEcheck.ps1
@@ -1,4 +1,4 @@
-curl https://raw.githubusercontent.com/Kahvi-0/EDRAV_ENUM/main/AVEDRFingerPrints.txt -o AVEDRFingerPrints.txt
+$WebResponse = Invoke-WebRequest https://raw.githubusercontent.com/Kahvi-0/EDRAV_ENUM/main/AVEDRFingerPrints.txt
 
 $processes = (Get-WmiObject Win32_Process | Select-Object ProcessName | Out-String) -split "`r`n" -replace '\s','' | Where-Object {$_.trim() -ne ""} |  Sort-Object | Get-Unique
 $services = (wmic service get name | Out-String)  -split "\r?\n|\r" -replace '\s','' | Where-Object {$_.trim() -ne ""}
@@ -7,14 +7,12 @@ echo "------------------"
 echo "Checking processes"
 echo "------------------"
 foreach($line in $services) {
-    (Get-Content ./AVEDRFingerPrints.txt | Select-String -Pattern "`:$line$") -replace ':',' is running from process'
+     ($WebResponse.tostring() -split "[`r`n]" | Select-String -Pattern "`:$line$") -replace ':',' is running from process'
 }
 
 echo "-----------------"
 echo "Checking services"
 echo "-----------------"
 foreach($line in $processes) {
-    (Get-Content ./AVEDRFingerPrints.txt | Select-String -Pattern "`:$line$")  -replace ':',' is running from service'
+    ($WebResponse.tostring() -split "[`r`n]" | Select-String -Pattern "`:$line$")  -replace ':',' is running from service'
 }
-
-del AVEDRFingerPrints.txt
