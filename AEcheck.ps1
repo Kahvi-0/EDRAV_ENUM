094fe5d654b9d252e553d521898f381dfa18ba75
Kahvi-0xFF <46513413+Kahvi-0@users.noreply.github.com>
2025-03-06 12:31:41 -0500

    Update AEcheck.ps1

--- AEcheck.ps1
+++ AEcheck.ps1
@@ -7,12 +7,12 @@ echo "------------------"
 echo "Checking services"
 echo "------------------"
 foreach($line in $services) {
-     ($WebResponse.tostring() -split "[`r`n]" | Select-String -Pattern "`:$line$") -replace ':',' is running from process'
+     ($WebResponse.tostring() -split "[`r`n]" | Select-String -Pattern "`:$line$") -replace ':',' is running from process '
 }
 
 echo "-----------------"
 echo "Checking processes"
 echo "-----------------"
 foreach($line in $processes) {
-    ($WebResponse.tostring() -split "[`r`n]" | Select-String -SimpleMatch "`:$line")  -replace ':',' is running from service'
+    ($WebResponse.tostring() -split "[`r`n]" | Select-String -SimpleMatch "`:$line")  -replace ':',' is running from service '
 }
